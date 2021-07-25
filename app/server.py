bb7ec78bdf98dd170e59a653e76d7cd2b16d0ba9
Tue Tran Duy <tuetranduy@gmail.com>
2021-07-25 11:32:38 +0700

    Order Cancellation Features implemented

--- app/server.py
+++ app/server.py
@@ -5,25 +5,54 @@ from controllers import future_order_controller
 app = Flask(__name__, instance_relative_config=True)
 
 
-@app.get("/getPosition/<position>")
+@app.get("/getPositions/<position>")
 def get_position(position):
-    response = future_order_controller.get_position_by_symbol(position)
+    response = future_order_controller.get_all_opened_positions_by_symbol(position)
 
     return {
         'data': response
     }
 
 
-@app.post("/placeFutureOrder")
-def place_future_order():
-    symbol = request.form['symbol']
-    side = request.form['side']
-    order_type = request.form['orderType']
-    quantity = request.form['quantity']
-    price = request.form['price']
-    working_type = request.form['workingType']
+@app.post("/placeOneWayOrder")
+def place_one_way_order():
+    response = future_order_controller.place_order(request)
 
-    response = future_order_controller.place_future_order(symbol, side, order_type, quantity, price, working_type)
+    return {
+        'data': response
+    }
+
+
+@app.post("/placeHedgeOrder")
+def place_hedge_order():
+    response = future_order_controller.place_hedge_order(request)
+
+    return {
+        'data': response
+    }
+
+
+@app.delete("/cancelOrder")
+def cancel_order():
+    response = future_order_controller.cancel_order(request)
+
+    return {
+        'data': response
+    }
+
+
+@app.delete("/cancelAllOrders")
+def cancel_all_opened_orders():
+    response = future_order_controller.cancel_all_opened_orders(request)
+
+    return {
+        'data': response
+    }
+
+
+@app.delete("/cancelMultipleOrders")
+def cancel_multiple_orders():
+    response = future_order_controller.cancel_multiple_orders(request)
 
     return {
         'data': response
--- controllers/future_order_controller.py
+++ controllers/future_order_controller.py
@@ -6,7 +6,7 @@ from constants import constants
 from utils import time_utils, signature_utils
 
 
-def get_position_by_symbol(symbol):
+def get_all_opened_positions_by_symbol(symbol):
     headers = {'Content-Type': 'application/json', 'X-MBX-APIKEY': constants.API_KEY}
     query = {'symbol': symbol, 'timestamp': time_utils.get_current_timestamp()}
 
@@ -20,7 +20,14 @@ def get_position_by_symbol(symbol):
     return response.json()
 
 
-def place_future_order(symbol, side, order_type, quantity, price, working_type):
+def place_order(request):
+    symbol = request.form['symbol']
+    side = request.form['side']
+    order_type = request.form['orderType']
+    quantity = request.form['quantity']
+    price = request.form['price']
+    working_type = request.form['workingType']
+
     headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': constants.API_KEY}
     query = {'symbol': symbol, 'side': side, 'type': order_type, 'timeInForce': 'GTC', 'quantity': quantity,
              'price': price,
@@ -31,5 +38,77 @@ def place_future_order(symbol, side, order_type, quantity, price, working_type):
     payload.update(signature)
 
     response = requests.post(constants.REST_BASE_URL + '/fapi/v1/order', payload, headers=headers)
+
+    return response.json()
+
+
+def place_hedge_order(request):
+    symbol = request.form['symbol']
+    side = request.form['side']
+    position_side = request.form['positionSide']
+    order_type = request.form['orderType']
+    quantity = request.form['quantity']
+    price = request.form['price']
+    working_type = request.form['workingType']
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': constants.API_KEY}
+    query = {'symbol': symbol, 'side': side, 'positionSide': position_side, 'type': order_type, 'timeInForce': 'GTC',
+             'quantity': quantity, 'price': price, 'workingType': working_type,
+             'timestamp': time_utils.get_current_timestamp()}
+    signature = {'signature': signature_utils.create_signature(urllib.parse.urlencode(query))}
+
+    payload = dict(query)
+    payload.update(signature)
+
+    response = requests.post(constants.REST_BASE_URL + '/fapi/v1/order', payload, headers=headers)
+
+    return response.json()
+
+
+def cancel_order(request):
+    symbol = request.form['symbol']
+    order_id = request.form['orderId']
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': constants.API_KEY}
+    query = {'symbol': symbol, 'timeInForce': 'GTC', 'orderId': order_id,
+             'timestamp': time_utils.get_current_timestamp()}
+    signature = {'signature': signature_utils.create_signature(urllib.parse.urlencode(query))}
+
+    payload = dict(query)
+    payload.update(signature)
+
+    response = requests.delete(constants.REST_BASE_URL + '/fapi/v1/order', data=payload, headers=headers)
+
+    return response.json()
+
+
+def cancel_all_opened_orders(request):
+    symbol = request.form['symbol']
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': constants.API_KEY}
+    query = {'symbol': symbol, 'timeInForce': 'GTC', 'timestamp': time_utils.get_current_timestamp()}
+    signature = {'signature': signature_utils.create_signature(urllib.parse.urlencode(query))}
+
+    payload = dict(query)
+    payload.update(signature)
+
+    response = requests.delete(constants.REST_BASE_URL + '/fapi/v1/allOpenOrders', data=payload, headers=headers)
+
+    return response.json()
+
+
+def cancel_multiple_orders(request):
+    symbol = request.form['symbol']
+    order_id_list = request.form['orderIdList']
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': constants.API_KEY}
+    query = {'symbol': symbol, 'orderIdList': order_id_list, 'timeInForce': 'GTC',
+             'timestamp': time_utils.get_current_timestamp()}
+    signature = {'signature': signature_utils.create_signature(urllib.parse.urlencode(query))}
+
+    payload = dict(query)
+    payload.update(signature)
+
+    response = requests.delete(constants.REST_BASE_URL + '/fapi/v1/batchOrders', data=payload, headers=headers)
 
     return response.json()
