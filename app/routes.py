d889f69f76b7e366dbae597b91449fa8f3678f28
Tue Tran Duy <tuetranduy@gmail.com>
2021-07-28 14:33:39 +0700

    refactor

--- app/routes.py
+++ app/routes.py
@@ -9,141 +9,153 @@ from controllers import future_controller
 def get_position(position):
     try:
         response = future_controller.get_all_opened_positions_by_symbol(position)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.post("/placeOneWayOrder")
 def place_one_way_order():
     try:
         response = future_controller.place_order(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.post("/placeHedgeOrder")
 def place_hedge_order():
     try:
         response = future_controller.place_hedge_order(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.delete("/cancelOrder")
 def cancel_order():
     try:
         response = future_controller.cancel_order(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.delete("/cancelAllOrders")
 def cancel_all_opened_orders():
     try:
         response = future_controller.cancel_all_opened_orders(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.delete("/cancelMultipleOrders")
 def cancel_multiple_orders():
     try:
         response = future_controller.cancel_multiple_orders(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.post("/closePositionForOneWayOrder")
 def close_position_for_normal_order():
     try:
         response = future_controller.close_position_for_normal_order(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.post("/batchPositionForOneWayOrder")
 def batch_close_position_for_normal_order():
     try:
         response = future_controller.batch_close_position_for_normal_order(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.get("/order")
 def get_order():
     try:
         response = future_controller.get_order(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.get("/futureBalance")
 def get_future_balance():
     try:
         response = future_controller.get_balance()
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.post("/changePositionMode")
 def change_position_mode():
     try:
         response = future_controller.change_position_mode(request)
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
 
 
 @app.get("/getPositionMode")
 def get_position_mode():
     try:
         response = future_controller.get_position_mode()
+
+        return {
+            'data': response
+        }
+
     except Exception as e:
         capture_exception(e)
-
-    return {
-        'data': response
-    }
