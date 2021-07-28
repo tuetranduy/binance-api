cadb65f2264a390033f2319986736ce07eed914c
Tue Tran Duy <tuetranduy@gmail.com>
2021-07-28 23:12:49 +0700

    test CI passing

--- tests/test_index.py
+++ tests/test_index.py
@@ -3,4 +3,4 @@ from tests import client  # type: ignore
 
 def test_index_page_should_return_404(client):
     index = client.get('/')
-    assert index.status_code == 1
+    assert index.status_code == 404
