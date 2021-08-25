ac401db54546c4a2f592877abb4d32647d095021
Tue Tran Duy <tuetranduy@gmail.com>
2021-08-25 10:07:55 +0700

    changed to prod

--- constants/constants.py
+++ constants/constants.py
@@ -7,4 +7,4 @@ REST_BASE_URL_PROD = 'https://fapi.binance.com'
 WS_BASE_URL = 'wss://stream.binancefuture.com'
 API_KEY = env.get('API_KEY')
 API_SECRET = env.get('API_SECRET')
-TEST_MODE = True
+TEST_MODE = False
